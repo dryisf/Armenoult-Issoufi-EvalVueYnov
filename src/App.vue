73492df4f7709f5977ce005c3fa13be63278df59
thinkedou <r.enguehard@gmail.com>
2020-10-31 21:33:45 +0100

    font awesome for icons

--- src/App.vue
+++ src/App.vue
@@ -58,23 +58,6 @@
             <!-- Content -->
             <section class="container">
 
-                <div class="row mt-5 pb-5">
-                    <div class="col-4">
-                        <select class="form-control">
-                          <option>Science</option>
-                          <option>Classic</option>
-                        </select>
-                    </div>
-                    <div class="col-8">
-                        <ul class="list-group">
-                          <li class="list-group-item">Cras justo odio</li>
-                          <li class="list-group-item">Dapibus ac facilisis in</li>
-                          <li class="list-group-item">Morbi leo risus</li>
-                          <li class="list-group-item">Porta ac consectetur ac</li>
-                          <li class="list-group-item">Vestibulum at eros</li>
-                        </ul>
-                    </div>
-                </div>
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-
+import VueRouter from 'vue-router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import '@/assets/css/font-face.css';
+import '@/assets/css/all.min.css';
+
 import '@/assets/vendor/font-awesome-4.7/css/font-awesome.min.css';
 import '@/assets/vendor/font-awesome-5/css/fontawesome-all.min.css';
 
@@ -12,11 +14,50 @@ import '@/assets/vendor/mdi-font/css/material-design-iconic-font.min.css';
 import '@/assets/css/theme.css';
 
 
-
 import App from './App.vue'
+Vue.use(VueRouter)
+
+
+const routes = [
+    {
+        path: '/', name:'login',
+        component: () => import('./components/LoginLayout.vue')
+    },
+    {
+        path: '/home', name:'home',
+        component: () => import('./components/CoffeeHome.vue')
+    },
+    {
+        path: '/login', name:'login',
+        component: () => import('./components/LoginLayout.vue')
+    },
+  {
+      path: '/products', name:'products',
+      component: () => import('./components/CoffeeProducts.vue')
+  },
+  {
+      path: '/products/:coffeeId', name:'productsDetails',
+      component: () => import('./components/CoffeeProductsDetails.vue')
+  },
+  {
+      path: '/about', name:'about',
+      component: () => import('./components/CoffeeAbout.vue')
+  },
+  {
+      path: '/store', name:'store',
+      component: () => import('./components/CoffeeStore.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
