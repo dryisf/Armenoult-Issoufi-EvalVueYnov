923c30d036b00e620034803ae2cef575d6d5fb0a
thinkedou <r.enguehard@gmail.com>
2020-11-01 17:40:18 +0100

    d

--- src/main.js
+++ src/main.js
@@ -18,46 +18,9 @@ import App from './App.vue'
 Vue.use(VueRouter)
 
 
-const routes = [
-    {
-        path: '/', name:'login',
-        component: () => import('./components/LoginLayout.vue')
-    },
-    {
-        path: '/home', name:'home',
-        component: () => import('./components/CoffeeHome.vue')
-    },
-    {
-        path: '/login', name:'login',
-        component: () => import('./components/LoginLayout.vue')
-    },
-  {
-      path: '/products', name:'products',
-      component: () => import('./components/CoffeeProducts.vue')
-  },
-  {
-      path: '/products/:coffeeId', name:'productsDetails',
-      component: () => import('./components/CoffeeProductsDetails.vue')
-  },
-  {
-      path: '/about', name:'about',
-      component: () => import('./components/CoffeeAbout.vue')
-  },
-  {
-      path: '/store', name:'store',
-      component: () => import('./components/CoffeeStore.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
-})
-
-
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  router
 }).$mount('#app')
